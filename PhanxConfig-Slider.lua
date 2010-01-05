f185c79f86ac9076b2a81a8c94fefa711ab8e603
Phanx-28751 <addons@phanx.net>
2010-01-05 10:33:10 +0000

    - Change draw layers for dropdown elements to facilitate adding textures to dropdown entries - Add "not intended for public use" notes to all files

--- PhanxConfig-Slider.lua
+++ PhanxConfig-Slider.lua
@@ -3,6 +3,10 @@
 	Simple slider widget generator.
 	Based on tekKonfig-Slider and AceGUI-3.0-Slider.
 	Requires LibStub.
+
+	This library is not intended for use by other authors. Absolutely no
+	support of any kind will be provided for other authors using it, and
+	its internals may change at any time without notice.
 ----------------------------------------------------------------------]]
 
 local lib, oldminor = LibStub:NewLibrary("PhanxConfig-Slider", 3)
