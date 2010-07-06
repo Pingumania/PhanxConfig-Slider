8641f3177811570106911d53e0bb1d640ee72a5a
Phanx-28751 <addons@phanx.net>
2010-07-06 02:41:29 +0000

    - Add svn:keywords "Revision" for automatic minor version incrementing

--- PhanxConfig-Slider.lua
+++ PhanxConfig-Slider.lua
@@ -9,7 +9,9 @@
 	its internals may change at any time without notice.
 ----------------------------------------------------------------------]]
 
-local lib, oldminor = LibStub:NewLibrary("PhanxConfig-Slider", 4)
+local MINOR_VERSION = tonumber(("$Revision$"):match("%d+"))
+
+local lib, oldminor = LibStub:NewLibrary("PhanxConfig-Slider", MINOR_VERSION)
 if not lib then return end
 
 local function OnEnter(self)
