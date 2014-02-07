3244b141ebc03c40144264783f4d93ff338502e0
Phanx-28751 <addons@phanx.net>
2014-02-07 22:29:53 +0000

    - Many consistent, all sides!

--- PhanxConfig-Slider.lua
+++ PhanxConfig-Slider.lua
@@ -14,85 +14,104 @@ local MINOR_VERSION = tonumber(strmatch("$Revision$", "%d+"))
 local lib, oldminor = LibStub:NewLibrary("PhanxConfig-Slider", MINOR_VERSION)
 if not lib then return end
 
-local scripts = {
-	OnEnter = function(self)
-		local text = self:GetParent().desc
-		if text then
-			GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-			GameTooltip:SetText(text, nil, nil, nil, nil, true)
-		end
-	end,
-	OnLeave = function(self)
-		GameTooltip:Hide()
-	end,
-	OnMouseWheel = function(self, delta)
-		local step = self:GetValueStep() * delta
-		local minValue, maxValue = self:GetMinMaxValues()
-		if step > 0 then
-			self:SetValue(min(self:GetValue() + step, maxValue))
-		else
-			self:SetValue(max(self:GetValue() + step, minValue))
-		end
-	end,
-	OnValueChanged = function(self)
-		local parent = self:GetParent()
-		local value = self:GetValue()
+------------------------------------------------------------------------
 
-		local valueStep = self.valueStep
-		if valueStep and valueStep > 0 then
-			local minValue = self.minValue or 0
-			value = floor((value - minValue) / valueStep + 0.5) * valueStep + minValue
-		end
+local scripts = {}
 
-		if self.valueFactor then
-			value = value / self.valueFactor
-		end
-
-		local handler = parent.ApplyValue or parent.OnValueChanged
-		if handler then
-			value = handler(parent, value) or value
-		end
-
-		if parent.isPercent then
-			parent.valueText:SetFormattedText("%.0f%%", value * 100)
-		else
-			parent:SetText(value)
-		end
+function scripts:OnEnter()
+	local text = self:GetParent().tooltipText
+	if text then
+		GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+		GameTooltip:SetText(text, nil, nil, nil, nil, true)
 	end
-}
+end
+function scripts:OnLeave()
+	GameTooltip:Hide()
+end
 
-local methods = {
-	GetText = function(self)
-		return self.valueText:GetText()
-	end,
-	SetText = function(self, text)
-		return self.valueText:SetText(text)
-	end,
+function scripts:OnMouseWheel(delta)
+	local step = self:GetValueStep() * delta
+	local minValue, maxValue = self:GetMinMaxValues()
+	if step > 0 then
+		self:SetValue(min(self:GetValue() + step, maxValue))
+	else
+		self:SetValue(max(self:GetValue() + step, minValue))
+	end
+end
 
-	GetLabel = function(self)
-		return self.labelText:GetText()
-	end,
-	SetLabel = function(self, text)
-		return self.labelText:SetText(text)
-	end,
+function scripts:OnValueChanged()
+	local parent = self:GetParent()
+	local value = self:GetValue()
+	local minValue, maxValue = self:GetMinMaxValues()
 
-	GetValue = function(self)
-		return self.slider:GetValue()
-	end,
-	SetValue = function(self, value)
-		if self.isPercent then
-			self.valueText:SetFormattedText("%.0f%%", value * 100)
-		else
-			self:SetText(value)
-		end
+	local valueStep = self.valueStep
+	if valueStep and valueStep > 0 then
+		value = floor((value - minValue) / valueStep + 0.5) * valueStep + minValue
+	end
 
-		if self.slider.valueFactor then
-			value = value * self.slider.valueFactor
-		end
+	if self.valueFactor then
+		value = value / self.valueFactor
+	end
 
-		return self.slider:SetValue(value)
-	end,
-}
+	local func = parent.func or parent.ApplyValue or parent.OnValueChanged
+	if func then
+		value = func(parent, value) or value
+	end
+
+	if parent.isPercent then
+		parent.valueText:SetFormattedText("%.0f%%", value * 100)
+	else
+		parent:SetText(value)
+	end
+end
+
+------------------------------------------------------------------------
+
+local methods = {}
+
+function methods:GetValue()
+	return self.slider:GetValue()
+end,
+function methods:SetValue(value)
+	if self.isPercent then
+		self.valueText:SetFormattedText("%.0f%%", value * 100)
+	else
+		self:SetText(value)
+	end
+
+	if self.slider.valueFactor then
+		value = value * self.slider.valueFactor
+	end
+
+	return self.slider:SetValue(value)
+end
+
+function methods:GetLabel()
+	return self.labelText:GetText()
+end
+function methods:SetLabel(text)
+	return self.labelText:SetText(text)
+end
+
+function methods:GetText()
+	return self.valueText:GetText()
+end
+function methods:SetText(text)
+	return self.valueText:SetText(text)
+end
+
+function methods:GetTooltipText()
+	return self.tooltipText
+end
+function methods:SetTooltipText(text)
+	self.tooltipText = text
+end
+
+function methods:SetFunction(func)
+	self.func = func
+end
+
+------------------------------------------------------------------------
 
 local function EditBox_OnEnterPressed(self) -- print("OnEnterPressed SLIDER")
 	local parent = self:GetParent():GetParent()
@@ -111,7 +130,7 @@ local function EditBox_OnEnterPressed(self) -- print("OnEnterPressed SLIDER")
 end
 
 local function EditBoxContainer_SetFormattedText(self, text, ...)
-	return self.editbox:SetText(text:format(...))
+	return self.editbox:SetFormattedText(text, ...)
 end
 
 local sliderBG = {
@@ -121,10 +140,10 @@ local sliderBG = {
 	insets = { left = 3, right = 3, top = 6, bottom = 6 }
 }
 
-function lib:New(parent, name, desc, lowvalue, highvalue, valuestep, percent, noEditBox)
+function lib:New(parent, name, tooltipText, lowvalue, highvalue, valuestep, percent, noEditBox)
 	assert(type(parent) == "table" and parent.CreateFontString, "PhanxConfig-Slider: Parent is not a valid frame!")
 	if type(name) ~= "string" then name = nil end
-	if type(desc) ~= "string" then desc = nil end
+	if type(tooltipText) ~= "string" then tooltipText = nil end
 	if type(lowvalue) ~= "number" then lowvalue = 0 end
 	if type(highvalue) ~= "number" then highvalue = 100 end
 	if type(valuestep) ~= "number" then valuestep = 1 end
@@ -132,8 +151,6 @@ function lib:New(parent, name, desc, lowvalue, highvalue, valuestep, percent, no
 	local frame = CreateFrame("Frame", nil, parent)
 	frame:SetWidth(186)
 	frame:SetHeight(42)
-
-	frame.desc = desc
 
 	frame.bg = frame:CreateTexture(nil, "BACKGROUND")
 	frame.bg:SetAllPoints(true)
@@ -152,7 +169,6 @@ function lib:New(parent, name, desc, lowvalue, highvalue, valuestep, percent, no
 	label:SetPoint("TOPLEFT", frame, 5, 0)
 	label:SetPoint("TOPRIGHT", frame, -5, 0)
 	label:SetJustifyH("LEFT")
-	label:SetText(name)
 
 	local low = slider:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
 	low:SetPoint("TOPLEFT", slider, "BOTTOMLEFT", 0, 3)
@@ -172,7 +188,7 @@ function lib:New(parent, name, desc, lowvalue, highvalue, valuestep, percent, no
 
 	local value
 	if not noEditBox and LibStub("PhanxConfig-EditBox", true) then
-		value = LibStub("PhanxConfig-EditBox"):New(frame, nil, desc, 5)
+		value = LibStub("PhanxConfig-EditBox"):New(frame, nil, tooltipText, 5)
 		value:SetPoint("TOP", slider, "BOTTOM", 0, 13)
 		value:SetWidth(100)
 		value.editbox:SetScript("OnEnter", scripts.OnEnter)
@@ -217,10 +233,12 @@ function lib:New(parent, name, desc, lowvalue, highvalue, valuestep, percent, no
 
 	frame.isPercent = percent
 
-	frame.GetText = methods.GetText
-	frame.SetText = methods.SetText
-	frame.GetValue = methods.GetValue
-	frame.SetValue = methods.SetValue
+	for name, func in pairs(methods) do
+		frame[name] = func
+	end
+
+	label:SetText(name)
+	frame.tooltipText = tooltipText
 
 	return frame
 end
